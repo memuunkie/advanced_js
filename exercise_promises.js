46c4f36216bd94915c85bc79cdd892efadb1901e
muunkie <6616957+memuunkie@users.noreply.github.com>
2018-02-20 18:33:42 -0800

    Initial code of star wars function

--- exercise_promises.js
+++ exercise_promises.js
@@ -6,3 +6,21 @@ function getMostFollowers(...usernames) {
 		return `${max.name} has the most followers with ${max.followers}`;
 	});
 }
+
+function starWarsString(id) {
+	var str = '';
+	return $.getJSON(`https://swapi.co/api/people/${id}/`).then(function(data){
+		str += `${data.name} is featured in `;
+		let filmData = data.films[0];
+		return $.getJSON(filmData);
+	}).then(function(res) {
+		str += `${res.title}, directed by ${res.director} `;
+		let planetData = res.planets[0];
+		return $.getJSON(planetData);
+	}).then(function(res) {
+		str += `and it takes place on ${res.name}`;
+		return str;
+	}).then(function(finalString) {
+		return finalString;
+	});
+}
