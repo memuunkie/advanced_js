c2c1ae83412233b95d822a939716b0657a2dd096
muunkie <6616957+memuunkie@users.noreply.github.com>
2018-02-20 19:10:24 -0800

    Added notes to code to track the process of .then

--- exercise_promises.js
+++ exercise_promises.js
@@ -1,26 +1,38 @@
+// use rest parameters
 function getMostFollowers(...usernames) {
 	let baseUrl = 'https://api.github.com/users/';
+	// map the array of usernames, return a .getJSON
 	let urls = usernames.map(username => $.getJSON(baseUrl + username));
+	// promise all the urls
 	return Promise.all(urls).then(function(data){
+		// sorts the data, returns the largest at index[0]
 		let max = data.sort((a,b) => a.followers < b.followers)[0];
 		return `${max.name} has the most followers with ${max.followers}`;
 	});
 }
 
 function starWarsString(id) {
+	// to hold all string additions
 	var str = '';
+	// make the call to the api
 	return $.getJSON(`https://swapi.co/api/people/${id}/`).then(function(data){
+		// adds to string
 		str += `${data.name} is featured in `;
+		// data.films is an array of api urls
 		let filmData = data.films[0];
 		return $.getJSON(filmData);
 	}).then(function(res) {
+		// gets the result from the call to filmData url
 		str += `${res.title}, directed by ${res.director} `;
+		// res.planets is an array of api urls
 		let planetData = res.planets[0];
 		return $.getJSON(planetData);
 	}).then(function(res) {
+		// gets the result from the call to planetData url
 		str += `and it takes place on ${res.name}`;
 		return str;
 	}).then(function(finalString) {
+		// gets the finished str from the previous 'then'
 		return finalString;
 	});
 }
